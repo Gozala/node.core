9a9df9b83df5b722b1787d6d1df8fb936222899f
Irakli Gozalishvili <rfobic@gmail.com>
2014-03-27 15:07:35 -0700

    Attempt to setup testing.

--- project.clj
+++ project.clj
@@ -5,27 +5,27 @@
   :url "https://github.com/gozala/node.core"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138" :scope "provided"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
   :source-paths ["src"]
 
   :plugins [[lein-cljsbuild "1.0.1"]
-            [com.cemerick/clojurescript.test "0.2.1"]]
+            [com.cemerick/clojurescript.test "0.3.0"]]
 
   :cljsbuild
   {:builds
-   [{:id "simple"
+   [{:id "simple-test"
      :source-paths ["src" "test"]
-     :compiler {:optimizations :simple
+     :compiler {:optimizations :whitespace
                 :pretty-print true
-                :output-to "target/cljs/test-simple.js"}}
-    {:id "advanced"
+                :output-to "target/cljs/test.js"}}
+    {:id "advanced-test"
      :source-paths ["src" "test"]
      :compiler {:optimizations :advanced
                 :pretty-print false
                 :static-fns true
                 :output-dir "out"
-                :output-to "target/cljs/test-advanced.js"
-                :source-map "target/cljs/test-advanced.js.map"}}]
+                :output-to "target/cljs/test.js"
+                :source-map "target/cljs/test.js.map"}}]
+
    :test-commands {"unit-tests" ["node" :node-runner
-                                 "this.literal_js_was_evaluated=true"
-                                 "target/cljs/testable.js"]}})
+                                 "target/cljs/test.js"]}})
