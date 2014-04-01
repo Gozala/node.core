f83173b879dba45a2092b5784ebe651b35d5966f
Irakli Gozalishvili <rfobic@gmail.com>
2014-03-31 20:04:26 -0700

    write process tests.

--- test/node/process.cljs
+++ test/node/process.cljs
@@ -2,6 +2,7 @@
   (:require [cemerick.cljs.test :as test]
             [clojure.string :as string]
             [node.process :refer [process]]
+            [node.path :as path]
             [node.utils :refer [json->edn]])
   (:require-macros [cemerick.cljs.test
                     :refer [is are deftest done with-test
@@ -19,7 +20,9 @@
 (deftest process-api
   (are [field type?] (type? (field @process))
        :command-line-arguments vector?
+       :command-line-arguments #(every? string? %)
        :exec-arguments vector?
+       :exec-arguments #(every? string? %)
        :exec-path string?
        :version string?
        :versions map?
@@ -59,3 +62,77 @@
 (deftest process-values
   (assert-values process js/process))
 
+(deftest process-static-fields
+  (are [field value]
+       (thrown? js/Error (swap! process assoc field value))
+       :command-line-arguments ["hello"]
+       :exec-arguments ["hello"]
+       :exec-path "5"
+       :version "5"
+       :versions nil
+       :versions {}
+       :config {}
+       :architecture :arm
+       :platform :win32))
+
+(deftest process-changes
+  (are [field value access]
+       (let [before (field @process)
+             ;; Store value since it may be expression
+             ;; and we want single execution.
+             after value
+
+             _! (swap! process assoc field after)
+             changed? (= after (access js/process))
+
+             _! (swap! process assoc field before)
+
+             changed-back? (= before (access js/process))]
+         (and (not= before after)
+              changed?
+              changed-back? ))
+
+       :title "testify" #(.-title %)
+
+       :working-directory
+       (path/directory (:working-directory @process))
+       #(.cwd %)
+
+       :max-next-depth
+       (+ (:max-next-depth @process) 17)
+       #(.-maxTickDepth %)
+
+       :mask
+       8r0777
+       #(.umask %)))
+
+(deftest node-changes
+  (are [field change value]
+       (let [before (field @process)
+             ;; Store value since it may be expression
+             ;; and we want single execution.
+             after value
+
+             _! (change js/process after)
+             changed? (= after (field @process))
+
+             _! (change js/process before)
+
+             changed-back? (= before (field @process))]
+         (and (not= before after)
+              changed?
+              changed-back? ))
+
+       :title #(set! (.-title %1) %2) "testify"
+
+       :working-directory
+       #(.chdir %1 %2)
+       (path/directory (:working-directory @process))
+
+       :max-next-depth
+       #(set! (.-maxTickDepth %1) %2)
+       (+ (:max-next-depth @process) 17)
+
+       :mask
+       #(.umask %1 %2)
+       8r0777))
